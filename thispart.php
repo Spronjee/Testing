00c57a2eb3f4f23bfff7ee74ab6bf3c1f82ab2f2
Ihor Chukhliebov <admin@MacBook-Pro-Ihor.local>
2019-10-06 15:21:23 +0300

    Adding objects Json

--- thispart.php
+++ thispart.php
@@ -11,12 +11,26 @@ class Person
 
 $person = new Person('Name', 'Surname');
 echo json_encode($person);
-$myfile = fopen("Info.txt", "w") or die("не удалось создать файл");
-$txt = "Name\n";
-fwrite($fd, $txt);
-$txt = "Surname";
-fwrite($myfile, $txt);
+$myfile = fopen("Info.json", "w") or die("не удалось создать файл");
+$json = "Name\n";
+fwrite($fd, $json);
+$json = "Surname";
+fwrite($myfile, $json);
 fclose($myfile);
 
+$option = $_GET['option'];
+
+if ( $option == 1 ) {
+    $data = [ 'a', 'b', 'c' ];
+    //  закодируем в массив JSON: ["a","b","c"]
+    // result[1] (returns "b")
+} else {
+    $data = [ 'name' => 'God', 'age' => -1 ];
+    //  закодируем в объект JSON:{"name":"God","age":-1}
+    //  result.name or result['name'] (returns "God")
+}
+
+header('Content-type: application/json');
+echo json_encode( $data );
 
 ?>
