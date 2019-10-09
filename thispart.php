a95433e8d74db275169458e96504ed46057a39c7
Ihor Chukhliebov <admin@MacBook-Pro-Ihor.local>
2019-10-09 20:55:29 +0300

    json

--- thispart.php
+++ thispart.php
@@ -1,36 +1,21 @@
 <?php
+
 class Person
 {
-    function __construct($name, $surname)
+    function __construct($login, $password)
     {
-        $this->name = $name;
-        $this->surname = $surname;
+        $this->login = $login;
+        $this->password = $password;
     }
 
 }
 
-$person = new Person('Name', 'Surname');
-echo json_encode($person);
-$myfile = fopen("Info.json", "w") or die("не удалось создать файл");
-$json = "Name\n";
-fwrite($fd, $json);
-$json = "Surname";
+$data = $_POST;
+
+$person = new Person($data['username'], $data['password']);
+$myfile = fopen("Info.json", "w+") or die("не удалось создать файл");
+$json = json_encode($person);
 fwrite($myfile, $json);
 fclose($myfile);
-
-$option = $_GET['option'];
-
-if ( $option == 1 ) {
-    $data = [ 'a', 'b', 'c' ];
-    //  закодируем в массив JSON: ["a","b","c"]
-    // result[1] (returns "b")
-} else {
-    $data = [ 'name' => 'God', 'age' => -1 ];
-    //  закодируем в объект JSON:{"name":"God","age":-1}
-    //  result.name or result['name'] (returns "God")
-}
-
-header('Content-type: application/json');
-echo json_encode( $data );
 
 ?>
